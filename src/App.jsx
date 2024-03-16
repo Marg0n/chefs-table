264c4bc14d78c111f220cd3840192d6f2d26a7d7
Noc1 PC MargoN <margon.dcosta@cccul.com>
2024-03-16 14:34:30 +0600

    added toster import

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Hero from "./components/Hero/Hero";
 import Nav from "./components/Nav/Nav";
 import Recipes from "./components/Recipes/Recipes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
-      <Recipes/>
+      <Recipes />
+
+      <ToastContainer />
     </>
   );
 }
