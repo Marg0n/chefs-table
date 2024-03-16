eec0280afed7d3b50a8d21c7683cddeb95b46700
MarGoN <monondcosta@gmail.com>
2024-03-17 01:35:20 +0600

    prymary work done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,19 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const Cart = ({ item }) => {
-  console.log("from cart", item);
+//   console.log("from cart", item);
 
   //   item.map((item) => (console.log(item.recipe_name)))
+
+  const [preparing, setPreparing] = useState([]);
+
+  const handlePreparing = (i) => {
+
+    // cost isExists = item.find
+    const newPreparing = [...preparing, i];
+    setPreparing(newPreparing);
+  };
 
   return (
     <div className="bg-gray-200 shadow-xl p-8 rounded-2xl">
@@ -12,7 +22,7 @@ const Cart = ({ item }) => {
           Want to cook: {item.length}
         </h3>
         <div className="divider"></div>
-        <table className="table-fixed w-full text-base  text-left">
+        <table className="table-fixed w-full text-base  text-left border-spacing-y-4 border-separate">
           <thead>
             <tr>
               <th className="w-8"></th>
@@ -24,18 +34,21 @@ const Cart = ({ item }) => {
           </thead>
 
           <tbody className="">
-            {item.map((item, idx) => (
+            {item.map((i, idx) => (
               <tr key={idx} className="">
                 <td>{idx + 1}. </td>
-                <td>{item.recipe_name}</td>
+                <td>{i.recipe_name}</td>
                 <td>
-                  {item.preparing_time} <small>minutes</small>
+                  {i.preparing_time} <small>minutes</small>
                 </td>
                 <td>
-                  {item.calories} <small>calories</small>
+                  {i.calories} <small>calories</small>
                 </td>
                 <td>
-                  <button className="btn btn-accent rounded-3xl">
+                  <button
+                    onClick={() => handlePreparing(i)}
+                    className="btn btn-accent rounded-3xl"
+                  >
                     Preparing
                   </button>
                 </td>
@@ -45,7 +58,48 @@ const Cart = ({ item }) => {
         </table>
       </div>
 
-      <div className=""></div>
+      <div className="mt-12">
+        <h3 className="text-xl font-semibold text-center">
+          Currently cooking: 02
+        </h3>
+        <div className="divider"></div>
+        <table className="table-fixed w-full text-base  text-left border-spacing-y-4 border-separate">
+          <thead>
+            <tr>
+              <th className="w-10"></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+            </tr>
+          </thead>
+
+          <tbody className="">
+            {preparing.map((p, idx) => (
+              <tr key={idx} className="">
+                <td>{idx + 1}. </td>
+                <td>{p.recipe_name}</td>
+                <td>
+                  {p.preparing_time} <small>minutes</small>
+                </td>
+                <td>
+                  {p.calories} <small>calories</small>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+
+          <tfoot className="">
+            <tr>
+              <td></td>
+              <td></td>
+              <td>
+                Total Time = {preparing.reduce((p, c) => p + c.preparing_time, 0)} minutes
+              </td>
+              <td>Total Calories = {preparing.reduce((p, c) => p + c.calories, 0)} calories</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 };
