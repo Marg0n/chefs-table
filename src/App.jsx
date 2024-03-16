0f8bd147828bec7133090b5fedf0f6874135c939
Noc1 PC MargoN <margon.dcosta@cccul.com>
2024-03-16 14:29:32 +0600

    added toster

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero/Hero";
 import Nav from "./components/Nav/Nav";
+import Recipes from "./components/Recipes/Recipes";
 
 function App() {
   return (
     <>
       <Nav />
       <Hero />
+      <Recipes/>
     </>
   );
 }
---
+++ src/components/Recipes/Recipes.jsx
@@ -0,0 +1,19 @@
+
+
+const Recipes = () => {
+  return (
+    <div className="container mx-auto mt-16">
+      <div className="text-center mt-4 space-y-4">
+        <h3 className="text-3xl font-semibold">Our Recipes</h3>
+        <p>
+          Choose the items of your liking. We have more items than you like! Try
+          it or order it!
+        </p>
+      </div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default Recipes;
